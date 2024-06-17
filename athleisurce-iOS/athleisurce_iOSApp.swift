cd1feb88e0b205dd1e34c7630d2381858d9bfda7
Ben Do <bdo.appworkshop@gmail.com>
2024-06-17 12:17:49 -0500

    Initial Commit

---
+++ athleisurce-iOS/athleisurce_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  athleisurce_iOSApp.swift
+//  athleisurce-iOS
+//
+//  Created by Ben Do on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct athleisurce_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
